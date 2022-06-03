9d5406ce3475435a7b59d72054ee5f9b1130f50a
danderle <danderle86@gmail.com>
2022-06-03 06:40:28 +0200

    Implemented the game board design and initial game start

---
+++ Blazor_Backgammon/ViewModels/BackgammonViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Blazor_Backgammon.ViewModels;
+
+public class BackgammonViewModel
+{
+    public int TotalFieldSpaces => 24;
+    public int HalfFieldSpaces => TotalFieldSpaces / 2;
+    public List<List<int>> GameField { get; set; } = new();
+
+    public BackgammonViewModel()
+    {
+        SetupGame();
+    }
+
+    public void SetupGame()
+    {
+        GameField.Clear();
+        GameField.Add(new List<int>() { 1,1 });
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>() { 2, 2, 2, 2, 2});
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>() { 2, 2, 2});
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>() { 1, 1, 1, 1, 1});
+        GameField.Add(new List<int>() { 2, 2, 2, 2, 2});
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>() { 1, 1, 1 });
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>() { 1, 1, 1, 1, 1});
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>());
+        GameField.Add(new List<int>() { 2, 2 });
+    }
+}
