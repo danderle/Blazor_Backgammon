1d6b137b9e81ecf52e98fc0a3009d669a5447b9f
danderle <danderle86@gmail.com>
2022-06-08 07:08:49 +0200

    Added the Player.cs, Chip.cs and chip.css files

---
+++ Blazor_Backgammon/DataModels/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace Blazor_Backgammon.DataModels
+{
+    /// <summary>
+    /// The player data model
+    /// </summary>
+    public enum Player
+    {
+        /// <summary>
+        /// Player 1
+        /// </summary>
+        One,
+
+        /// <summary>
+        /// Player 2
+        /// </summary>
+        Two
+    }
+}
---
+++ Blazor_Backgammon/Models/Chip.cs
@@ -0,0 +1,38 @@
+﻿using Blazor_Backgammon.DataModels;
+
+namespace Blazor_Backgammon.Models
+{
+    /// <summary>
+    /// The Chip class
+    /// </summary>
+    public class Chip
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Which player
+        /// </summary>
+        public Player Player { get; private set; }
+
+        /// <summary>
+        /// Flag for selecting a chip
+        /// </summary>
+        public bool IsSelected { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="player"></param>
+        public Chip(Player player)
+        {
+            Player = player;
+        } 
+        
+        #endregion
+
+    }
+}
