fd62bd04d1da28c884248fe94abf09b754014825
danderle <danderle86@gmail.com>
2022-06-08 07:24:13 +0200

    Implemented the starting position and selecting a chip

--- Blazor_Backgammon/ViewModels/BackgammonViewModel.cs
+++ Blazor_Backgammon/ViewModels/BackgammonViewModel.cs
@@ -1,42 +1,143 @@
-﻿namespace Blazor_Backgammon.ViewModels;
+﻿using Blazor_Backgammon.DataModels;
+using Blazor_Backgammon.Models;
 
+namespace Blazor_Backgammon.ViewModels;
+
+/// <summary>
+/// The view model for the <see cref="Backgammon.razor"/> page
+/// </summary>
 public class BackgammonViewModel
 {
-    public int TotalFieldSpaces => 24;
-    public int HalfFieldSpaces => TotalFieldSpaces / 2;
-    public List<List<int>> GameField { get; set; } = new();
+    #region Public Properties
 
+    /// <summary>
+    /// The total number of spaces a player can move
+    /// </summary>
+    public int TotalFieldSpaces => 24;
+
+    /// <summary>
+    /// The number of spaces of either the top or bottom of the game board
+    /// </summary>
+    public int HalfFieldSpaces => TotalFieldSpaces / 2;
+
+    /// <summary>
+    /// The current active player
+    /// </summary>
+    public Player ActivePlayer { get; set; }
+
+    /// <summary>
+    /// The game field with all the chips
+    /// </summary>
+    public List<List<Chip>> GameField { get; set; } = new();
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
     public BackgammonViewModel()
+    {
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Gets a players chips at a field index
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public List<Chip> GetPlayerChipsAtIndex(int index)
+    {
+        if (GameField.Count > 0)
+        {
+            return GameField[index];
+        }
+        return new List<Chip>();
+    }
+
+    #endregion
+
+    #region Command Methods
+
+    /// <summary>
+    /// The start / reset of the game
+    /// </summary>
+    public void StartGame()
     {
         SetupGame();
     }
 
-    public void SetupGame()
+    /// <summary>
+    /// Select a chip to move positions
+    /// </summary>
+    /// <param name="chip"></param>
+    public void ClickedChip(Chip chip)
     {
-        GameField.Clear();
-        GameField.Add(new List<int>() { 1,1 });
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>() { 2, 2, 2, 2, 2});
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>() { 2, 2, 2});
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>() { 1, 1, 1, 1, 1});
-        GameField.Add(new List<int>() { 2, 2, 2, 2, 2});
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>() { 1, 1, 1 });
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>() { 1, 1, 1, 1, 1});
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>());
-        GameField.Add(new List<int>() { 2, 2 });
+        if (chip.Player != ActivePlayer)
+        {
+            return;
+        }
+
+        chip.IsSelected ^= true;
     }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Creates a list of player chips
+    /// </summary>
+    /// <param name="numberToCreate">The number of chips to create</param>
+    /// <param name="player">The player</param>
+    /// <returns>The list of chips</returns>
+    private List<Chip> CreateChips(int numberToCreate, Player player)
+    {
+        var chips = new List<Chip>();
+        for (int i = 0; i < numberToCreate; i++)
+        {
+            chips.Add(new Chip(player));
+        }
+        return chips;
+    }
+
+    
+    /// <summary>
+    /// The Setup for the game start position
+    /// </summary>
+    private void SetupGame()
+    {
+        ActivePlayer = Player.One;
+        GameField.Clear();
+        GameField.Add(CreateChips(2, Player.One));
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(CreateChips(5, Player.Two));
+        GameField.Add(new List<Chip>());
+        GameField.Add(CreateChips(3, Player.Two));
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(CreateChips(5, Player.One));
+        GameField.Add(CreateChips(5, Player.Two));
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(CreateChips(3, Player.One));
+        GameField.Add(new List<Chip>());
+        GameField.Add(CreateChips(5, Player.One));
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(new List<Chip>());
+        GameField.Add(CreateChips(2, Player.Two));
+    }
+
+    #endregion
 }
