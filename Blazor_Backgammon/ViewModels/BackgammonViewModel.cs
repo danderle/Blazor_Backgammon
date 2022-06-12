f21018ff46952b82d8408db73b568d3cb24f4c2f
danderle <dennis.anderle@soprasteria.com>
2022-06-12 07:33:00 +0200

    Both player can move their chips across the board

--- Blazor_Backgammon/ViewModels/BackgammonViewModel.cs
+++ Blazor_Backgammon/ViewModels/BackgammonViewModel.cs
@@ -17,6 +17,7 @@ public class BackgammonViewModel
 
     #region Public Properties
 
+    public bool HideButton { get; set; }
 
     /// <summary>
     /// The total number of spaces a player can move
@@ -42,6 +43,8 @@ public class BackgammonViewModel
     /// 
     /// </summary>
     public List<Dice> Dice { get; set; } = new ();
+
+    public List<int> DiceNumbers { get; set; } = new ();
 
     #endregion
 
@@ -102,43 +105,44 @@ public class BackgammonViewModel
         if (chip.IsMoveOption)
         {
             chip.IsMoveOption = false;
-            foreach (Dice dice in Dice)
+            foreach (int number in DiceNumbers)
             {
-                if (chip.MoveOption.DiceNumber == dice.Number)
+                if (chip.MoveOption.DiceNumber == number)
                 {
-                    dice.IsSet = true;
                     chip.MoveOption.IsSet = true;
+                    chip.IsMoveOption = false;
+                    DiceNumbers.Remove(number);
+                    break;
                 }
             }
+
+            if (chip.MoveOption.DiceNumber == DiceNumbers.Sum())
+            {
+                chip.MoveOption.IsSet = true;
+                chip.IsMoveOption = false;
+                DiceNumbers.Clear();
+            }
+
+            RemoveSelectedChipAndChipOptions();
+            if (DiceNumbers.Count == 0)
+            {
+                HideButton = false;
+                SwitchPlayer();
+            }
+
             return;
         }
 
         var chips = GetPlayerChipsAtIndex(chip.FieldIndex);
         chips.Last().IsSelected ^= true;
-        chipIsActive ^= true;
 
-        ShowPossibleOptions(chip.FieldIndex);
-    }
-
-    private void ShowPossibleOptions(int fieldIndex)
-    {
-        if (Dice[0].Number != Dice[1].Number)
+        if (chips.Last().IsSelected)
         {
-            if (ActivePlayer == Player.One)
-            {
-                foreach (var dice in Dice)
-                {
-                    var moveOption = fieldIndex + dice.Number;
-                    if (moveOption < TotalFieldSpaces)
-                    {
-                        var chipsOtherPlayer = GameField[fieldIndex].Count(item => item.Player != ActivePlayer);
-                        if (chipsOtherPlayer < 2)
-                        {
-                            GameField[moveOption].Add(new Chip(moveOption, ActivePlayer, new MoveOption(dice.Number)));
-                        }
-                    }
-                }
-            }
+            ShowPossibleOptions(chip.FieldIndex);
+        }
+        else
+        {
+            RemoveSelectedChipAndChipOptions();
         }
     }
 
@@ -150,7 +154,16 @@ public class BackgammonViewModel
         foreach (var dice in Dice)
         {
             dice.Roll();
+            DiceNumbers.Add(dice.Number);
         }
+
+        if (Dice[0].Number == Dice[1].Number)
+        {
+            DiceNumbers.Add(Dice.Last().Number);
+            DiceNumbers.Add(Dice.Last().Number);
+        }
+
+        HideButton = true;
     }
 
     #endregion
@@ -172,6 +185,157 @@ public class BackgammonViewModel
             chips.Add(new Chip(fieldIndex, player));
         }
         return chips;
+    }
+
+    /// <summary>
+    /// Switches the active player
+    /// </summary>
+    private void SwitchPlayer()
+    {
+        if (ActivePlayer == Player.One)
+        {
+            ActivePlayer = Player.Two;
+        }
+        else
+        {
+            ActivePlayer = Player.One;
+        }
+    }
+
+    /// <summary>
+    /// After selecting a move option the selection will be removed and
+    /// the other move options will also be removed
+    /// </summary>
+    private void RemoveSelectedChipAndChipOptions()
+    {
+        var chipsToRemove = new List<Chip>();
+        foreach (var chipList in GameField)
+        {
+            foreach (var chip in chipList)
+            {
+                if (chip.Player != ActivePlayer)
+                {
+                    continue;
+                }
+
+                if (chip.IsSelected || chip.IsMoveOption)
+                {
+                    chipsToRemove.Add(chip);
+                }
+            }
+
+            lock (chipList)
+            {
+                foreach (var chip in chipsToRemove)
+                {
+                    chipList.Remove(chip);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="fieldIndex"></param>
+    private void ShowPossibleOptions(int fieldIndex)
+    {
+        if ((DiceNumbers.Count > 1 && DiceNumbers[0] != DiceNumbers[1]) || DiceNumbers.Count == 1)
+        {
+            foreach (var number in DiceNumbers)
+            {
+                if (ActivePlayer == Player.One)
+                {
+                    var moveOption = fieldIndex + number;
+                    if (moveOption < TotalFieldSpaces)
+                    {
+                        var chipsOtherPlayer = GameField[moveOption].Count(item => item.Player != ActivePlayer);
+                        if (chipsOtherPlayer < 2)
+                        {
+                            GameField[moveOption].Add(new Chip(moveOption, ActivePlayer, new MoveOption(number)));
+                        }
+                    }
+                }
+                else
+                {
+                    var moveOption = fieldIndex - number;
+                    if (moveOption >= 0)
+                    {
+                        var chipsOtherPlayer = GameField[moveOption].Count(item => item.Player != ActivePlayer);
+                        if (chipsOtherPlayer < 2)
+                        {
+                            GameField[moveOption].Add(new Chip(moveOption, ActivePlayer, new MoveOption(number)));
+                        }
+                    }
+                }
+            }
+
+            if (DiceNumbers.Count > 1)
+            {
+                var sum = DiceNumbers.Sum();
+                if (ActivePlayer == Player.One)
+                {
+                    var sumOption = fieldIndex + sum;
+                    if (sumOption < TotalFieldSpaces)
+                    {
+                        var chipsOtherPlayer = GameField[sumOption].Count(item => item.Player != ActivePlayer);
+                        if (chipsOtherPlayer < 2)
+                        {
+                            GameField[sumOption].Add(new Chip(sumOption, ActivePlayer, new MoveOption(sum)));
+                        }
+                    }
+                }
+                else
+                {
+                    var sumOption = fieldIndex - sum;
+                    if (sumOption >= 0)
+                    {
+                        var chipsOtherPlayer = GameField[sumOption].Count(item => item.Player != ActivePlayer);
+                        if (chipsOtherPlayer < 2)
+                        {
+                            GameField[sumOption].Add(new Chip(sumOption, ActivePlayer, new MoveOption(sum)));
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (ActivePlayer == Player.One)
+            {
+                int sum = 0;
+                foreach (var number in DiceNumbers)
+                {
+                    sum += number;
+                    var moveOption = fieldIndex + sum;
+                    if (moveOption < TotalFieldSpaces)
+                    {
+                        var chipsOtherPlayer = GameField[moveOption].Count(item => item.Player != ActivePlayer);
+                        if (chipsOtherPlayer < 2)
+                        {
+                            GameField[moveOption].Add(new Chip(moveOption, ActivePlayer, new MoveOption(number)));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                int sum = 0;
+                foreach (var number in DiceNumbers)
+                {
+                    sum += number;
+                    var moveOption = fieldIndex - sum;
+                    if (moveOption >= 0)
+                    {
+                        var chipsOtherPlayer = GameField[moveOption].Count(item => item.Player != ActivePlayer);
+                        if (chipsOtherPlayer < 2)
+                        {
+                            GameField[moveOption].Add(new Chip(moveOption, ActivePlayer, new MoveOption(number)));
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /// <summary>
